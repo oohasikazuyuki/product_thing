96d56d530faa41ef468abaa7e140008af8014bca
oohasikazuyuki <cdok1230245@gn.iwasaki.ac.jp>
2024-05-20 11:28:53 +0900

    first commit

--- html/product_thing/src/Controller/APIController.php
+++ html/product_thing/src/Controller/APIController.php
@@ -2,9 +2,33 @@
 namespace App\Controller;
 use Cake\Core\Configure;
 use Cake\Http\Client;
+use Cake\Log\Log;
+
 
 class APIController extends AppController
 {
+
+
+    /*
+        public function initialize(): void
+        {
+            parent::initialize();
+            $this->loadComponent('RequestHandler');
+        }
+
+        public function index(): void
+        {
+            $this->viewBuilder()->setLayout('API');
+        }
+
+
+  */
+
+    public function initialize(): void
+    {
+        parent::initialize();
+        $this->loadComponent('RequestHandler');
+    }
 
     public function index(): void
     {
@@ -12,40 +36,118 @@ class APIController extends AppController
     }
 
 
-  public function rEstateAPI(): void
-{
-    $Baseurl ="https://www.reinfolib.mlit.go.jp/ex-api/external/XIT001?";
-    $API_KEY = getenv('API_KEY');
-    error_log($API_KEY);
-    $param = [
-        'year' => 2021,
-        'priceClassification' => 01,
-         'area' => 13101,
-    ];
-    $url = $Baseurl . http_build_query($param);
-    $http  = new Client();
-   // echo $url;
-    $response = $http->get($url, [], [
-        'headers' => ['Ocp-Apim-Subscription-Key' => $API_KEY]
-    ]);
+    public function rEstateAPI()
+    {
+        $base_url = 'https://www.reinfolib.mlit.go.jp/ex-api/external/XIT001?year=2015&quarter=2&city=13102&priceClassification=01';
+
+        $query = ['year'=>'2015','quarter'=>'2','city'=>'13102','priceClassification'=>'01'];
+        $header = array(
+            'Content-Type: application/x-www-form-urlencoded',
+            'Context-Length: ' . 20,
+            'Ocp-Apim-Subscription-Key: 2f8763d2bb7e41feb2485d92d1e426c4'
+        );
+        $content = array(
+            'http' => array(
+                'method' => 'GET',
+                'header' => implode("\r\n", $header),
+                'content' => "",
+                'ignore_errors' => true
+            )
+        );
+
+        $context = stream_context_create($content);
+
+        $response = file_get_contents(
+            $base_url,false,$context);
+       // file_put_contents('/var/www/html/product_thing/logs/aaa.zip', $response, FILE_APPEND);
+        pr(json_decode(gzdecode($response),true));
 
 
+// https://qiita.com/api/v2/tags/PHP/items?page=1&per_page=5
 
-   // echo $url;
-    if($response->isOk()){
-        $body = $response->getStringBody();
-        $data = json_decode($body, true);
-        $this->set('data', $data);
-    } else {
-        $this->Flash->error('APIの取得に失敗しました');
-        $this->Flash->error($response->getStringBody());
-        if ($response->getStatusCode() == 401) {
-            $this->Flash->error('APIキーが無効または設定されていません。');
-        } elseif ($response->getStatusCode() == 400) {
-            $this->Flash->error('APIリクエストのパラメータが正しく設定されていません。');
-        } elseif ($response->getStatusCode() == 404) {
-            $this->Flash->error('指定したURLに該当するAPIが存在しません。');
-        }
+// 結果はjson形式で返されるので
+      //  $result = json_decode($response,true);
+        pr($base_url.http_build_query($query));
     }
-}
+    public function phpinfo()
+    {
+        echo  phpinfo();
+
+    }
+
+
+    public function selectAPI()
+    {
+
+
+
+
+        $base_url = 'https://www.reinfolib.mlit.go.jp/ex-api/external/XIT002?area=13';
+
+
+
+
+        $query = ['year'=>'2015','quarter'=>'2','city'=>'13102','priceClassification'=>'01'];
+        $header = array(
+            'Content-Type: application/x-www-form-urlencoded',
+            'Context-Length: ' . 20,
+            'Ocp-Apim-Subscription-Key: 2f8763d2bb7e41feb2485d92d1e426c4'
+        );
+        $content = array(
+            'http' => array(
+                'method' => 'GET',
+                'header' => implode("\r\n", $header),
+                'content' => "",
+                'ignore_errors' => true
+            )
+        );
+
+        $context = stream_context_create($content);
+
+        $response = file_get_contents(
+            $base_url,false,$context);
+
+        $decode_response = json_decode(gzdecode($response),true);
+
+        for($i = 0; $i < count($decode_response['data']); $i++){
+            echo var_export($decode_response['data'][$i]['name'],true);
+        }
+
+
+       // echo var_export($decode_response['data'][1]['name'],true);
+
+
+        // file_put_contents('/var/www/html/product_thing/logs/aaa.zip', $response, FILE_APPEND);
+     //   pr(json_decode(gzdecode($response),true));
+
+
+   //     echo $decode_response;
+
+    }
+
+    public function GetPrefecturesAndCities($area = null)
+    {
+        $url = 'https://www.reinfolib.mlit.go.jp/ex-api/external/XIT002?';
+        $params = [
+            'area' => $area
+        ];
+        $header = array(
+            'Content-Type: application/x-www-form-urlencoded',
+            'Context-Length: ' . 20,
+            'Ocp-Apim-Subscription-Key: 2f8763d2bb7e41feb2485d92d1e426c4'
+        );
+        $content = array(
+            'http' => array(
+                'method' => 'GET',
+                'header' => implode("\r\n", $header),
+                'content' => "",
+                'ignore_errors' => true
+            )
+        );
+
+        $context = stream_context_create($content);
+        $response  = file_get_contents($url,false,$context);
+        echo $response[1];
+
+    }
 }
---
+++ html/product_thing/src/Controller/Component/APIComponent.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller\Component;
+
+use Cake\Controller\Component;
+use Cake\Http\Client;
+
+class APIComponent extends Component
+{
+
+    const API_KEY = '2f8763d2bb7e41feb2485d92d1e426c4';
+    const BASE_URL = 'https://www.reinfolib.mlit.go.jp/ex-api/external/XIT001?year=2015&area=13';
+
+    public function rEstateAPI(): string|bool
+    {
+        phpinfo();
+
+
+        $c_i = curl_init();
+        curl_setopt($c_i, CURLOPT_URL, self::BASE_URL);
+        curl_setopt($c_i, CURLOPT_POST, true);
+        // curl_setopt($c_i,CURLOPT_FIELDS, $param);
+        curl_setopt($c_i, CURLOPT_HTTPHEADER, $head);
+        $res = curl_exec($c_i);
+        curl_close($c_i);
+        return $res;
+        $data = http_build_query(self::BASE_URL, "", "&");
+        $header = array(
+            "Content-Type: application/x-www-form-urlencoded",
+            "Context-Length: " . strlen($data),
+            "Ocp-Apim-Subscription-Key: " . self::API_KEY
+        );
+
+        $context = array(
+            "http" => array(
+                "method" => "POST",
+                "header" => implode("\r\n", $header),
+                "content" => $data,
+                'ignore_errors' => true
+            )
+        );
+        file_get_contents(self::BASE_URL, false, stream_context_create($context));
+
+    }
+}
